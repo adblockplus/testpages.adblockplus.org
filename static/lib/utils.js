09639c16d2730e522fe8402d6711b150eb0d1e0c
tonimellodic <tonimellodic@gmail.com>
2020-04-20 15:40:51 +0200

    Fixes #18 - allow expected screenshots page view

--- static/lib/utils.js
+++ static/lib/utils.js
@@ -12,3 +12,30 @@ function removeWaitingElements()
     }
   }
 }
+
+function expectedScreenshotsParam()
+{
+  let url = new URL(window.location.href);
+  return url.searchParams.get("expected") == "1";
+}
+
+function expectedScreenshotsView()
+{
+  let documents = [document];
+  while (documents.length > 0)
+  {
+    let doc = documents.shift();
+    doc.body.classList.add("expected");
+    for (let i = 0; i < doc.defaultView.frames.length; i++)
+    {
+      try
+      {
+        documents.push(doc.defaultView.frames[i].document);
+      }
+      catch (e) {}
+    }
+  }
+}
+
+if (expectedScreenshotsParam())
+  window.addEventListener("DOMContentLoaded", expectedScreenshotsView);
