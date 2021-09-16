da07bb15dc6b5c0f8de071b1d8ebc8d79c9baae0
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2021-09-16 14:41:10 +0200

    Fix lint errors

--- static/testfiles/script/eh-script.js
+++ static/testfiles/script/eh-script.js
@@ -1,15 +1,14 @@
 "use strict";
-var testCases = ["div", "p", "input", "img"];
 
-testCases.forEach(elementType => {
-    let target = document.getElementById(`${elementType}-script-target`);
-    let newElement = document.createElement(elementType);
-    newElement.innerHTML = "Failed. Script ran and was applied to the page.";
-    newElement.setAttribute("data-expectedresult", "fail");
-    newElement.setAttribute("aria-label", "script-fail");
-    newElement.id = `${elementType}-eh`;
-    target.appendChild(newElement);
+const testCases = ["div", "p", "input", "img"];
+
+testCases.forEach(elementType =>
+{
+  let target = document.getElementById(`${elementType}-script-target`);
+  let newElement = document.createElement(elementType);
+  newElement.innerHTML = "Failed. Script ran and was applied to the page.";
+  newElement.setAttribute("data-expectedresult", "fail");
+  newElement.setAttribute("aria-label", "script-fail");
+  newElement.id = `${elementType}-eh`;
+  target.appendChild(newElement);
 });
-
-
-
