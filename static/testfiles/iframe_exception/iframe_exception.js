e65be439416871f642a5a453c87ebb8d263861d7
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-03-07 21:38:31 +0100

    added adjustments

--- static/testfiles/iframe_exception/iframe_exception.js
+++ static/testfiles/iframe_exception/iframe_exception.js
@@ -2,8 +2,7 @@
 
 let parentFrame = document.getElementById("script-target");
 let iframe = document.createElement("iframe");
-//iframe.src = "https://allowed.abptestpages.org/static/testfiles/iframe_exception/parent.html";
-iframe.src = "/testfiles/iframe_exception/parent.html";
-iframe.width = "620";
-iframe.height = "620";
+iframe.src = "https://allowed.abptestpages.org/static/testfiles/iframe_exception/parent.html";
+iframe.width = "100";
+iframe.height = "100";
 parentFrame.appendChild(iframe);
