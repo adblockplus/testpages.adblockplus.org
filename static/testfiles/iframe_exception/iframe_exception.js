c5d50b7ddb9c51751f5689f6b7a3df2793d54b29
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-03-08 13:22:24 +0100

    Next changes

--- static/testfiles/iframe_exception/iframe_exception.js
+++ static/testfiles/iframe_exception/iframe_exception.js
@@ -2,7 +2,7 @@
 
 let parentFrame = document.getElementById("script-target");
 let iframe = document.createElement("iframe");
-iframe.src = "https://allowed.abptestpages.org/static/testfiles/iframe_exception/parent.html";
-iframe.width = "100";
-iframe.height = "100";
+iframe.src = "https://allowed.abptestpages.org/testfiles/iframe_exception/parent.html";
+iframe.width = "300";
+iframe.height = "300";
 parentFrame.appendChild(iframe);
