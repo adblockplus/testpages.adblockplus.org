ae126ca6e30d7fc67d915c53301b4c1691a25fa8
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2022-03-08 09:17:24 +0100

    remove unneded file

--- static/testfiles/iframe_exception/iframe_exception.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-let parentFrame = document.getElementById("script-target");
-let iframe = document.createElement("iframe");
-iframe.src = "https://allowed.abptestpages.org/testfiles/iframe_exception/parent.html";
-iframe.width = "100";
-iframe.height = "100";
-parentFrame.appendChild(iframe);
