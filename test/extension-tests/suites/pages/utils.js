7b6c5ae6e7497bc3319b1212fe1340d7c51bc206
Agnieszka Czyzewska <a.czyzewska@eyeo.com>
2022-06-06 13:21:40 +0200

    use `100 and above

--- test/extension-tests/suites/pages/utils.js
+++ test/extension-tests/suites/pages/utils.js
@@ -39,7 +39,7 @@ export function isExcluded(page, browserName, browserVersion) {
     excluded = {MicrosoftEdge: "", msedge: "", firefox: "", chrome: ""};
   // https://gitlab.com/eyeo/adblockplus/abc/testpages.adblockplus.org/-/issues/105
   else if (page == "filters/rewrite")
-    excluded = {firefox: ""};
+    excluded = {firefox: ">100.0"};
   // https://gitlab.com/eyeo/adblockplus/adblockpluschrome/-/issues/306#note_484130304
   else if (page == "filters/webrtc" || page == "exceptions/webrtc")
     excluded = {MicrosoftEdge: "", msedge: "", firefox: "", chrome: ""};
