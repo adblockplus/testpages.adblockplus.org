a06e2f053a633f17be453abd22bc2da8dd616aa8
Krystian Zlomek <k.zlomek@eyeo.com>
2022-09-05 15:27:08 +0200

    fixup! Excluded check in Firefox due to bug
    Signed-off-by: Krystian Zlomek <k.zlomek@eyeo.com>

--- test/extension-tests/pages/utils.js
+++ test/extension-tests/pages/utils.js
@@ -35,6 +35,9 @@ export function isExcluded(page, browserName, browserVersion) {
   else if (page == "exceptions/iframe_subdomains")
     excluded = {MicrosoftEdge: "", msedge: "", firefox: "", chrome: ""};
   // https://gitlab.com/eyeo/adblockplus/abc/testpages.adblockplus.org/-/issues/105
+  else if (page == "exceptions/iframe")
+    excluded = {firefox: ""};
+  // https://gitlab.com/eyeo/adblockplus/abc/testpages.adblockplus.org/-/issues/121
   else if (page == "filters/rewrite")
     excluded = {firefox: ">=100.0"};
   // https://gitlab.com/eyeo/adblockplus/adblockpluschrome/-/issues/306#note_484130304
