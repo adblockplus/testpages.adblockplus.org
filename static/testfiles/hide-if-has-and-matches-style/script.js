be5b679001e5eeb9004c3461f3bb2bd6f23cc7bb
Maciej Pawłowski <m.pawlowski@eyeo.com>
2023-10-13 11:06:42 +0200

    DPD-2179: Ensure the test fails if a filter blocks a legit element
    Previously, if the snippet blocked the legit element, the span that says
    "Should not be hidden" disappeared, but the test remained visibly green,
    there were no indicated failures.
    
    Now, we observe changes to the element and insert a "fail" label to the
    page if the element becomes hidden.
    
    To verify, adjust the "Legit elements" filter to match:
    
    hide-if-has-and-matches-style a[href="#comments-target-ad"] div[id] span.label ';' /\\bdisplay:\ none\;/

--- static/testfiles/hide-if-has-and-matches-style/script.js
+++ static/testfiles/hide-if-has-and-matches-style/script.js
@@ -7,6 +7,31 @@ function addTarget(id, expectedResult, innerHTML) {
   target.setAttribute("aria-label", expectedResult);
   target.innerHTML = innerHTML;
   document.getElementById(`${id}-target`).appendChild(target);
+  return target.id;
+}
+
+function verifyTargetRemainsVisible(targetNode, callWhenHidden) {
+  // Observe attribute changes:
+  const mutationObserverConfig = {attributes: true};
+
+  // If the style.display changes to 'none', the snippet has
+  // hidden the element. Notify callWhenHidden().
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "attributes") {
+        if (targetNode.style.display == "none") {
+          callWhenHidden();
+          observer.disconnect();
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, mutationObserverConfig);
 }
 
 setTimeout(() => {
@@ -18,11 +43,19 @@ setTimeout(() => {
     `<span>Failed. Element is not hidden.<br></span>
       <span class='label'><a href='#basic-target-ad'>basic-target-ad</a></span>`
   );
-  addTarget(
+  let visibleElementId = addTarget(
     "comments",
     "pass",
     `<span>Should not be hidden</span>
       <span class='label' style='display: none'>
       <a href='#comments-target-ad'>comments-target-ad</a></span>`
   );
+
+  // If the target that should not have been hidden was hidden,
+  // insert a new "fail" element to indicate a failure.
+  let targetNode = document.getElementById(visibleElementId);
+  let failureCallback = () => {
+    addTarget("comments", "fail", "<span>Element was hidden by snippet, despite mismatched searchStyle</span>");
+  };
+  verifyTargetRemainsVisible(targetNode, failureCallback);
 }, 500);
