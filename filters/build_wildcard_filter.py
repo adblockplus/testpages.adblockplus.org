350112cfccd9f4131db32add2d8816096599a1be
Anton Smirnov <a.smirnov@eyeo.com>
2024-02-07 17:35:18 +0500

    Fix the linting issue

--- filters/build_wildcard_filter.py
+++ filters/build_wildcard_filter.py
@@ -1,9 +1,5 @@
-import urllib.parse
-
-
 def build_wildcard_filter(domain):
     if domain == "localhost":
         return "abptestpages.*"
-  
     subdomains = domain.split(".")
     return subdomains[-2] + ".*"
