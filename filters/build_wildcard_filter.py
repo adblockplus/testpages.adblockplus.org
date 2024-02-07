93826974c40d7d7c2515d4ebb3e6a08b09d0476d
Anton Smirnov <a.smirnov@eyeo.com>
2024-02-07 18:44:15 +0500

    Add a comment [skip ci]

--- filters/build_wildcard_filter.py
+++ filters/build_wildcard_filter.py
@@ -1,4 +1,7 @@
 def build_wildcard_filter(domain):
+    # If you run it locally the web server is available on localhost:5001,
+    # but the wildcard test assumes a domain to be used. To make it happen
+    # "local.abptestpages.org" is resolved to localhost with DNS.
     if domain == "localhost":
         return "abptestpages.*"
     subdomains = domain.split(".")
