3199492db2651afe7d636a6f1377de7d594ff5e2
Aga <a.czyzewska@eyeo.com>
2024-05-23 13:24:39 +0200

    Fix firefox way of handling errors

--- test/extension-tests/pages/index.js
+++ test/extension-tests/pages/index.js
@@ -49,6 +49,7 @@ async function updateFilters(driver, extensionHandle, url) {
           errors = await browser.runtime.sendMessage(
             {type: "filters.importRaw", text: filtersToAdd}
           );
+          errors = errors[0];
         }
         else if (typeof chrome != "undefined") { // Chromium
           errors = await new Promise(resolve => chrome.runtime.sendMessage(
