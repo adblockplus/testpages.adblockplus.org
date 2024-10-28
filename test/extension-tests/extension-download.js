1668fa012ee3e24cc7a43ac87bcb39c1942b4ad3
Ursa Kacar <kacar.ursa@gmail.com>
2024-10-28 18:05:41 +0100

    removes test JSON

--- test/extension-tests/extension-download.js
+++ test/extension-tests/extension-download.js
@@ -22,70 +22,15 @@ import semver from "semver";
 
 import {download} from "@eyeo/get-browser-binary";
 
-// const URL = "https://gitlab.com/api/v4/projects/62936800/releases"; // rowan
-// const URL = "https://gitlab.com/api/v4/projects/59518842/releases"; // ext releases
-
-// TEST JSON as those changes are not yet in releases repo
-const info = [
-  {
-    "name": "AdBlock 99.99.9",
-    "tag_name": "adblock-99.99.9",
-    "description": null,
-    "created_at": "2024-10-28T13:08:27.696Z",
-    "released_at": "2024-10-28T13:08:27.696Z",
-    "upcoming_release": false,
-    "assets": {
-      "count": 9,
-      "links": [
-        {
-          "id": 5120561,
-          "name": "adblockbeta-chrome-44f3b275-mv3.zip",
-          "url": "https://gitlab.com/-/project/62936800/uploads/fc8615b2b90d05e570ca64e98bf931ca/adblockbeta-chrome-44f3b275-mv3.zip",
-          "direct_asset_url": "https://gitlab.com/r.deysel/extensions/-/releases/adblock-99.99.9/downloads/adblockbeta-chrome-44f3b275-mv3.zip",
-          "link_type": "other"
-        },
-        {
-          "id": 5120553,
-          "name": "adblock-firefox-44f3b275-mv2.xpi",
-          "url": "https://gitlab.com/-/project/62936800/uploads/dcc67b17fc4b24dc137a37a7dda71d24/adblock-firefox-44f3b275-mv2.xpi",
-          "direct_asset_url": "https://gitlab.com/r.deysel/extensions/-/releases/adblock-99.99.9/downloads/adblock-firefox-44f3b275-mv2.xpi",
-          "link_type": "other"
-        },
-        {
-          "id": 5120550,
-          "name": "adblock-chrome-44f3b275-mv3.zip",
-          "url": "https://gitlab.com/-/project/62936800/uploads/95b0eb135ef57360456df1fe79702719/adblock-chrome-44f3b275-mv3.zip",
-          "direct_asset_url": "https://gitlab.com/r.deysel/extensions/-/releases/adblock-99.99.9/downloads/adblock-chrome-44f3b275-mv3.zip",
-          "link_type": "other"
-        },
-        {
-          "id": 5120544,
-          "name": "adblock-chrome-44f3b275-mv2.zip",
-          "url": "https://gitlab.com/-/project/62936800/uploads/ff7a038f9e77398c910bdd292ae05892/adblock-chrome-44f3b275-mv2.zip",
-          "direct_asset_url": "https://gitlab.com/r.deysel/extensions/-/releases/adblock-99.99.9/downloads/adblock-chrome-44f3b275-mv2.zip",
-          "link_type": "other"
-        },
-        {
-          "id": 5120542,
-          "name": "adblock-44f3b275.tar.gz",
-          "url": "https://gitlab.com/-/project/62936800/uploads/8dba84010d9cc7cfadf0d87bf9130c72/adblock-44f3b275.tar.gz",
-          "direct_asset_url": "https://gitlab.com/r.deysel/extensions/-/releases/adblock-99.99.9/downloads/adblock-44f3b275.tar.gz",
-          "link_type": "other"
-        }
-      ]
-    },
-    "_links": {
-      "closed_issues_url": "https://gitlab.com/r.deysel/extensions/-/issues?release_tag=adblock-99.99.9&scope=all&state=closed",
-    }
-  }
-];
+const URL = "https://gitlab.com/api/v4/projects/62936800/releases"; // rowan ("test env")
+// const URL = "https://gitlab.com/api/v4/projects/59518842/releases"; // ext releases ("production")
 
 async function run() {
-  // const info = await got(URL).json();
+  const info = await got(URL).json();
 
   let manifestVersion = process.env.MANIFEST_VERSION || "2";
 
-  // this will get the first occurence of the file, but that should be good enough, as releases are shown from the most recent one to the oldest one
+  // this will get the first occurence of the file, but that should be good enough?, as releases are shown from the most recent to the oldest one
   let extensionLinkObject = info[0].assets.links.find(link =>
     link.direct_asset_url &&
       link.direct_asset_url.includes("adblock-chrome") && // this is currently adblock, because there is no adblockplus build available yet with this new formatting
@@ -98,9 +43,8 @@ async function run() {
   await download(`${extensionLink}?inline=false`, archive);
   await extractZip(archive, {dir: testext});
   // eslint-disable-next-line no-console
-  console.log(`${extensionLink} extracted to ${testext}`);
+  console.log(`${filename} extracted to ${testext}`);
 }
 
 run();
 console.log("Downloading ABP extension..."); // eslint-disable-line no-console
-
