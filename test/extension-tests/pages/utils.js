e77950068bd888779cb7f610752c16242f25eb76
Aga <a.czyzewska@eyeo.com>
2025-01-03 12:03:34 +0100

    extend timeout

--- test/extension-tests/pages/utils.js
+++ test/extension-tests/pages/utils.js
@@ -78,7 +78,7 @@ export async function runGenericTests(driver, expectedScreenshot, browserName,
     await driver.wait(async() => {
       actualScreenshot = await takeScreenshot(driver);
       return Jimp.diff(actualScreenshot, expectedScreenshot).percent == 0;
-    }, 20000, "Screenshots don't match", 200);
+    }, 40000, "Screenshots don't match", 200);
   }
 
   try {
