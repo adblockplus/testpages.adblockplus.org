a1edf0928466b8af259dc4a535d0a9c20e46ecca
Aga <a.czyzewska@eyeo.com>
2025-01-03 12:14:02 +0100

    Extend more

--- test/extension-tests/pages/utils.js
+++ test/extension-tests/pages/utils.js
@@ -78,7 +78,7 @@ export async function runGenericTests(driver, expectedScreenshot, browserName,
     await driver.wait(async() => {
       actualScreenshot = await takeScreenshot(driver);
       return Jimp.diff(actualScreenshot, expectedScreenshot).percent == 0;
-    }, 40000, "Screenshots don't match", 200);
+    }, 50000, "Screenshots don't match", 200);
   }
 
   try {
